3c868ae8b69384d9848041bc3c70a9128fe0d149
igorz5 <104494213+igorz5@users.noreply.github.com>
2022-10-26 01:54:45 +0300

    Carousel of reviews

--- src/js/app.js
+++ src/js/app.js
@@ -73,31 +73,82 @@ export default class App {
     this.map.events.add("click", this.onClick);
     document.addEventListener("submit", this.onDocumentSubmit);
 
-    const balloonContentLayout = ymaps.templateLayoutFactory.createClass(`
-      <div class="balloon">
-        <div class="balloon__address"></div>
-        <ul class="balloon__review-list">
-          {% for obj in properties.geoObjects %}
+    const openBalloon = (obj) => {
+      const reviews = this.storage.getData().filter((review) => {
+        const coordsA = obj.geometry.coordinates;
+        const coordsB = review.coords;
+        return coordsA[0] === coordsB[0] && coordsA[1] === coordsB[1];
+      });
+
+      const reviewsItems = [];
+      const { data } = obj.properties;
+
+      reviews.forEach((review) => {
+        const { name, place, date, message } = review;
+        const reviewLayout = `
           <li class="balloon__review-item">
             <div class="review">
               <div class="review__header">
-                <div class="review__name">{{obj.properties.data.name}}</div>
-                <div class="review__place">{{obj.properties.data.place}}</div>
-                <div class="review__date">{{obj.properties.data.date}}</div>
+                <div class="review__name">${name}</div>
+                <div class="review__place">${place}</div>
+                <div class="review__date">${date}</div>
               </div>
-              <p class="review__message">{{obj.properties.data.message}}</p>
+              <p class="review__message">${message}</p>
             </div>
           </li>
-          {% endfor %}
-        </ul>
-        ${formTemplate}
+        `;
+
+        reviewsItems.push(reviewLayout);
+      });
+
+      const layout = `
+        <div class="balloon">
+          <div class="balloon__address">${data.address}</div>
+          <ul class="balloon__review-list">
+            ${reviewsItems.join("")}
+          </ul>
+          ${formTemplate}
+        </div>
+      `;
+
+      this.map.balloon.open(data.coords, layout);
+    };
+
+    const balloonContentLayout = ymaps.templateLayoutFactory.createClass(
+      `
+      <div class="balloon">
+        <a href="#" class="balloon__link balloon__address-link">{{properties.data.address}}</a>
+        <div class="review">
+          <div class="review__header">
+            <div class="review__name">{{properties.data.name}}</div>
+            <div class="review__place">{{properties.data.place}}</div>
+            <div class="review__date">{{properties.data.date}}</div>
+          </div>
+          <p class="review__message">{{properties.data.message}}</p>
+        </div>
       </div>
-    `);
+    `,
+      {
+        build() {
+          this.constructor.superclass.build.call(this);
+
+          const link = this._element.querySelector(".balloon__address-link");
+          link.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            const obj = this.getData().geoObject;
+
+            openBalloon(obj);
+          });
+        },
+      }
+    );
 
     this.objectManager = new ymaps.ObjectManager({
       clusterDisableClickZoom: true,
       clusterize: true,
-      clusterBalloonContentLayout: balloonContentLayout,
+      clusterBalloonItemContentLayout: balloonContentLayout,
+      clusterBalloonContentLayout: "cluster#balloonCarousel",
     });
 
     this.objectManager.objects.events.add("click", (e) => {
@@ -212,6 +263,7 @@ export default class App {
           place: place,
           message: message,
           date: date,
+          address: address,
         },
       },
     };
